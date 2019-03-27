73eb8b92429631db966e68f15be8cb5fdd1d1669
AliRaza1844 <aliraza.cs15@gmail.com>
2019-03-27 15:23:47 +0500

    +1 Some final modifications

--- AnalogPen/urls.py
+++ AnalogPen/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('register/', user_views.register, name='register-page'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login-page'),
     path('home/', home_views.home, name='home-page'),
+    path('', home_views.home, name='home-page'),
     path('livetext/', home_views.live_text, name='livetext-page'),
-    path('generatetext/', home_views.generate_text, name='generate-text'),
+    path('textdocument/', home_views.doc_text, name='doc-page'),
+    path('textdocument2/', home_views.doc_text2, name='doc-page'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,9 +6,23 @@ from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 sys.path.append('/home/hina/Music/Django/handwriting-synthesis')
 import demo
 
+count = 0
 
 def home(request):
     return render(request, 'home/home.html')
+
+
+def doc_text2(request):
+    # text = request.POST.get('fileupload')
+    print("Converting text to handwriting...")
+    demo.generateDoc()
+
+    return render(request, 'home/textdocument.html')
+
+
+def doc_text(request):
+
+    return render(request, 'home/textdocument.html')
 
 
 @csrf_protect
@@ -19,11 +33,4 @@ def live_text(request):
     if not text == '':
         demo.generate(text)
 
-    return render(request, 'home/livetext.html')
-
-
-def generate_text(request):
-    text = request.POST.get('textarea')
-    print(text)
-    # demo.generate(text)
     return render(request, 'home/livetext.html')
